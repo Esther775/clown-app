f1e4bc2152d4cbb7359c02ddc1b8380f1e1a5e26
Esther Morris <EstherMorris775@gmail.com>
2022-08-11 14:14:48 -0400

    added restful routes for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,25 @@
 class BookingsController < ApplicationController
+  def index
+    booking = Booking.all
+
+    render json: booking.as_json
+  end
+
+  def show
+    booking = Booking.find_by(id: params[:id])
+
+    render json: booking.as_json
+  end
+
+  def create
+    booking = Booking.new(
+      clown_id: params[:clown_id],
+      user_id: params[:user_id],
+      time: params[:time],
+      price: params[:price],
+      event: params[:event],
+    )
+    booking.save
+    render json: booking.as_json
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(name: "lisa")
+    render json: user.as_json
+  end
 end
